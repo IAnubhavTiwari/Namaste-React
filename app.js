20cb22824d616465d2197235c62488139239af6b
IAnubhavTiwari <anubhavtiwarionline@gmail.com>
2025-01-05 18:29:31 +0530

    second commit

--- app.js
+++ app.js
@@ -14,18 +14,23 @@
  * ReactElement(Object) => HTML(Browser Understands)
  */
 
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 
 const parent = React.createElement("div", { id: "parent" }, [
-    React.createElement("div", { id: "child" }, [
-      React.createElement("h1", {}, "I'm an h1 tag"),
-      React.createElement("h2", {}, "I'm an h2 tag"),
-    ]),
-    React.createElement("div", { id: "child2" }, [
-      React.createElement("h1", {}, "I'm an h1 tag"),
-      React.createElement("h2", {}, "I'm an h2 tag"),
-    ]),
-  ]);
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "This is Namaste React 🚀"),
+    React.createElement("h2", {}, "by Anubhav Tiwari"),
+  ]),
+  React.createElement("div", { id: "child2" }, [
+    React.createElement("h1", {}, "I'm an h1 tag"),
+    React.createElement("h2", {}, "I'm an h2 tag"),
+  ]),
+]);
 
-    const root = ReactDOM.createRoot(document.getElementById("root"))
+console.log(parent); // object
 
-    root.render(parent)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
